c08fb27069c591718a6bef081d8ad2a2a9d8938b
Fede <chiellofederico@gmail.com>
2021-03-01 15:31:23 +0100

    Init

---
+++ index-php.php
@@ -0,0 +1,19 @@
+<!-- PHP -->
+<?php
+  include 'database.php';
+ ?>
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Dischi musicali</title>
+</head>
+<body>
+
+
+</body>
+</html>
