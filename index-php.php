847a9e70f785185a897c0d24f836dba6fcbd27cd
Fede <chiellofederico@gmail.com>
2021-03-01 21:25:39 +0100

    fine milestone 1

--- index-php.php
+++ index-php.php
@@ -22,19 +22,20 @@
   <!-- MAIN -->
   <main>
     <!-- container-box -->
-    <div class="container-box container">
-      <!-- box -->
-      <div class="box">
+    <div class="container-box">
+      <div class="container">
+        <!-- box -->
         <?php foreach ($dischi as $disco) {?>
-          <img src="<?php echo $disco['poster']; ?>" alt="">
-          <h2><?php echo $disco['title']; ?></h2>
-          <h3><?php echo $disco['author']; ?></h3>
-          <p><?php echo $disco['year']; ?></p>
+          <div class="box">
+            <img src="<?php echo $disco['poster']; ?>" alt="poster" class="pd_b-2">
+            <h2 class="pd_b-2"><?php echo $disco['title']; ?></h2>
+            <h3 class="pd_b-1"><?php echo $disco['author']; ?></h3>
+            <p class="pd_b-1"><?php echo $disco['year']; ?></p>
+          </div>
         <?php } ?>
       </div>
     </div>
   </main>
-
 
 </body>
 </html>
