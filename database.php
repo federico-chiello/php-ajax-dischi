1f4ad00f383269ab4a7d9646cd5e4e6348096e61
Fede <chiellofederico@gmail.com>
2021-03-02 15:53:38 +0100

    correzione del tag php dal database

--- database.php
+++ database.php
@@ -5,11 +5,11 @@ Al caricamento della pagina si dovranno visualizzare tutti i dischi. -->
 <?php
   $dischi = [
     [
-      'poster' => 'https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg',
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'genre' => 'Rock',
-      'year' => '1988'
+      "poster" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+      "title" => "New Jersey",
+      "author" => "Bon Jovi",
+      "genre" => "Rock",
+      "year" => "1988"
     ],
     [
       "poster"=> "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
@@ -75,4 +75,3 @@ Al caricamento della pagina si dovranno visualizzare tutti i dischi. -->
       "year"=> "1987"
     ]
   ];
- ?>
