8654557230c1569b1f9bdee04b3a7ab1ad9c1b31
Fede <chiellofederico@gmail.com>
2021-03-02 15:36:07 +0100

    utilizzo di axios in vue

--- src/script.js
+++ src/script.js
@@ -2,3 +2,17 @@
 // Attraverso l’utilizzo di Vue ed axios, al caricamento della pagina stampare i dischi musicali.
 // Bonus
 // Filtrare i dati per genere musicale.
+let app = new Vue({
+  el: '#music',
+  data:{
+    dischi: []
+  },
+  mounted(){
+    axios
+      .get('server.php')
+      .then( response => {
+        this.dischi = response.data;
+        console.log(this.dischi);
+      });
+  }
+});
